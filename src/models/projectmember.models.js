1d7c22844227459468134376ab0090ca9e58296c
uuuuday <krishnauday200301@gmail.com>
2025-07-10 20:08:41 +0530

    add all the schema models

---
+++ src/models/projectmember.models.js
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from "mongoose";
+import { AvaliableUserRoles, UserRolesEnum } from "../utils/constants.js";
+
+
+const projectMemberSchema = new Schema(
+    {
+        user:{
+            type:Schema.Types.ObjectId,
+            ref:"User",
+            required:true,
+        },
+
+        project:{
+            type:Schema.Types.ObjectId,
+            ref:"Project",
+            required:true,
+
+        },
+    role:{
+        type:String,
+        enum :AvaliableUserRoles,
+        default:UserRolesEnum.MEMBER,
+
+    },
+},
+    {
+        timestamps:true
+    },
+   
+)
+
+export const ProjectMemberSchema = mongoose.model(
+    "ProjectMemberSchema",
+    projectMemberSchema
+);
