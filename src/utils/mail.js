b8e34c5e6aa37afc4768c56d639c6be73d013ce6
uuuuday <krishnauday200301@gmail.com>
2025-07-15 20:00:25 +0530

    add new mail sending utility

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,77 +1,96 @@
 import nodemailer from 'nodemailer';
-
 import Mailgen from 'mailgen';
 
-//we are going to write in such a way its a utility function
 
 const sendMail = async(options)=>{
-
-
-    const mailGenerator = new Mailgen({
-        theme :"default",
-        product :{
-            name:"Task managner",
-            link:"hhtp://taskmanager.app" 
-        },
-    })
-
-
-    const emailTextual = mailGenerator.generatePlaintext(options.MailgenContent)
-    const emailHTML = mailGenerator.generate(options.MailgenContent)
-
-
-    //creating the transporter function which is responsible for sending the mail
-    const transporter = nodemailer.createTransport({
-        host:process.env.SMTP_HOST,
-        port:process.env.SMTP_PORT,
-        auth:{
-            user:process.env.SMTP_USER,
-            pass:process.env.SMTP_PASS,
-        }
-    })
-
-
-    const mail = {
-        from:"mail.taskmanager@example.com",
-        to:options.mail, //receviers mail address
-        subject:options.subject,
-        text:emailTextual,
-        html:emailHTML
+    var mailGenerator = new Mailgen({
+    theme: 'default',
+    product: {
+        // Appears in header & footer of e-mails
+        name: 'Mailgen',
+        link: 'https://mailgen.js/'
+        // Optional product logo
+        // logo: 'https://mailgen.js/img/logo.png'
     }
+});
+
+    //need to give both the options because some inboxes may not support HTML
+    const emailBody = mailGenerator.generate(options.mailgenContent);
+    const  emailText = mailGenerator.generatePlaintext(options.mailgenContent); 
+
+//nodmailer is used to send the mails //here in this case mailgen used to craft the mails
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
 
 
-    try {
-        await sendMail(mail)
-    } catch (error) {
-        console.error("plz check the credentials in .env file .some error in sending the email")
-    }
 
+const mail = await transporter.sendMail({
+    from: 'manager@gmail.com',
+    to: options.email,
+    subject: options.subject,
+    text: emailText, // plain‑text body
+    html: emailBody, // HTML body
+  });
+
+
+  try {
+    await transporter.sendMail(mail)
+  } catch (error) {
+    console.error("some error in sending the mail plz check again")
+  }
 }
 
 
-
-const emailVerficationMailGenContent =(username ,verificationurl)=>{
+const emailVerificationMailgenContent = async(username,verificationUrl)=>{
     return {
-        body :{
-            name:username,
-            intro :"Welcome to our app we are excited to have on board",
-            action:{
-                instructions:
-                    "To verify your email plz click on the following button",
-                button:{
-                    color:"#22BC66",
-                    text:"verify your email",
-                    link:verificationurl
-                },
-            },
-            outro :
-             "If you have anu queries regarding this plz click reply back to this email"
+    body: {
+      name: username,
+      intro: "Welcome to our app! We're very excited to have you on board.",
+      action: {
+        instructions:
+          "To verify your email please click on the following button:",
+        button: {
+          color: "#22BC66", // Optional action button color
+          text: "Verify your email",
+          link: verificationUrl,
         },
+      },
+      outro:
+        "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+  };
+}
 
-    };
-};
+
+const forgotPasswordMailgenContent = async(username,passwordResetUrl)=>{
+    return {
+    body: {
+      name: username,
+      intro: "We got a request to reset the password of our account",
+      action: {
+        instructions:
+          "To reset your password click on the following button or link:",
+        button: {
+          color: "#22BC66", // Optional action button color
+          text: "Reset password",
+          link: passwordResetUrl,
+        },
+      },
+      outro:
+        "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+  };
+}
 
 export {
     sendMail,
-    emailVerficationMailGenContent
+    emailVerificationMailgenContent,
+    forgotPasswordMailgenContent
 }
