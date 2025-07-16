ed5d70f533258ee5d44955e169ff718837ae90ca
uuuuday <krishnauday200301@gmail.com>
2025-07-16 20:46:06 +0530

    add the controller code

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import express from "express";
+import router from "./routes/auth.routes.js";
 
 
 const app = express();
 
-
+app.use('api/v1/users',router)
 export default app;
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/api-error.js";
 import { ApiResposne } from "../utils/api-response.js";
 import { asyncHandler } from "../utils/async-handler.js";
+import { sendMail } from "../utils/MAIL.JS";
 import { emailVerificationMailgenContent } from "../utils/MAIL.JS";
 const registerUser = asyncHandler(async (req, res) => {
   const { email, username, password, role } = req.body;
@@ -10,11 +11,11 @@ const registerUser = asyncHandler(async (req, res) => {
   //checking fot existingUser
 
   const existingUser = await User.findOne({
-     $or:[{email}, {username}]  //both email and username is given unique (databse design)
-  }) 
+    $or: [{ email }, { username }]  //both email and username is given unique (databse design)
+  })
 
-  if(existingUser){
-    throw new ApiError(202,"user already exists with this username and passowrd",false)
+  if (existingUser) {
+    throw new ApiError(202, "user already exists with this username and passowrd", false)
   }
 
   const user = await User.create({
@@ -24,14 +25,36 @@ const registerUser = asyncHandler(async (req, res) => {
     role
   })
 
-  if(!user){
-    throw new ApiError(202,"user not craeted plz check",false)
+  if (!user) {
+    throw new ApiError(202, "user not craeted plz check", false)
   }
- 
+  //we created a method in userschema to genearte a temporary token
+  const { unhashedToken, hashedToken, tokenexpiry } = await user.temporaryToken();
 
-  
-  
+  //saving the token in the database
+  user.emailVerificationToken = hashedToken;
+  user.emailVerificationExpiry = tokenexpiry;
 
+
+  await user.save();
+
+  const verificationUrl = `${process.env.Base_url}/verify/${unhashedToken}`;
+
+  const mailContent = await emailVerificationMailgenContent(user.username, verificationUrl);
+
+
+  await sendMail({
+    email: user.email,
+    subject: "verify your email",
+    mailgenContent: mailContent
+  })
+
+
+  return res.status(201).json(new ApiResponse(201, {
+    id: user._id,
+    username: user.username,
+    email: user.email,
+  }, "User registered successfully, please verify your email"));
 
 });
 
