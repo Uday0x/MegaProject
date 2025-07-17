919ca475d7110b09269b783ebabdc280f6852ec3
uuuuday <krishnauday200301@gmail.com>
2025-07-17 21:50:41 +0530

    add the lastest changes to the code

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import router from "./routes/auth.routes.js";
 
 
 const app = express();
+app.use(express.json());
 
-app.use('api/v1/users',router)
+app.use('/api/v1/users',router)
 export default app;
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -10,58 +10,99 @@ const registerUser = asyncHandler(async (req, res) => {
 
   //checking fot existingUser
 
-  const existingUser = await User.findOne({
-    $or: [{ email }, { username }]  //both email and username is given unique (databse design)
-  })
-
-  if (existingUser) {
-    throw new ApiError(202, "user already exists with this username and passowrd", false)
+  try {
+    const existingUser = await User.findOne({
+      $or: [{ email }, { username }]  //both email and username is given unique (databse design)
+    })
+  
+    if (existingUser) {
+      throw new ApiError(202, "user already exists with this username and passowrd", false)
+    }
+  
+    const user = await User.create({
+      email,
+      username,
+      password,
+      role
+    })
+  
+    if (!user) {
+      throw new ApiError(202, "user not craeted plz check", false)
+    }
+    //we created a method in userschema to genearte a temporary token
+    const { unhashedToken, hashedToken, tokenexpiry } = await user.temporaryToken();
+  
+    //saving the token in the database
+    user.emailVerificationToken = hashedToken;
+    user.emailVerificationExpiry = tokenexpiry;
+  
+  
+    await user.save();
+  
+    const verificationUrl = `${process.env.Base_url}/verify/${unhashedToken}`;
+  
+    const mailContent = await emailVerificationMailgenContent(user.username, verificationUrl);
+  
+  
+    await sendMail({
+      email: user.email,
+      subject: "verify your email",
+      mailgenContent: mailContent
+    })
+  
+  
+    return res.status(201).json(new ApiResponse(201, {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    }, "User registered successfully, please verify your email"));
+  
+  } catch (error) {
+    throw new ApiError(202,"something wnet wrong in registering the user",false)
   }
-
-  const user = await User.create({
-    email,
-    username,
-    password,
-    role
-  })
-
-  if (!user) {
-    throw new ApiError(202, "user not craeted plz check", false)
-  }
-  //we created a method in userschema to genearte a temporary token
-  const { unhashedToken, hashedToken, tokenexpiry } = await user.temporaryToken();
-
-  //saving the token in the database
-  user.emailVerificationToken = hashedToken;
-  user.emailVerificationExpiry = tokenexpiry;
-
-
-  await user.save();
-
-  const verificationUrl = `${process.env.Base_url}/verify/${unhashedToken}`;
-
-  const mailContent = await emailVerificationMailgenContent(user.username, verificationUrl);
-
-
-  await sendMail({
-    email: user.email,
-    subject: "verify your email",
-    mailgenContent: mailContent
-  })
-
-
-  return res.status(201).json(new ApiResponse(201, {
-    id: user._id,
-    username: user.username,
-    email: user.email,
-  }, "User registered successfully, please verify your email"));
-
 });
 
 const loginUser = asyncHandler(async (req, res) => {
-  const { email, username, password, role } = req.body;
+  try {
+    const { email, username, password, role } = req.body;
 
-  //validation
+    //validation //user login vlaidator we pass it in route
+    const user = User.findOne({
+      email
+    })
+
+    if (!user) {
+      throw new ApiError(202, "user not found", false)
+    }
+
+    //comapring the hashed passowrd 
+    const isMatch = await user.isPassowrdCorrect()
+
+    if (!isMatch) {
+      throw new ApiError(202, "passowrd not matching at the hashing level", false)
+    }
+
+
+    //setting the jwt token
+    const token = user.generateAcccesToken();
+
+    const cookieoptions = {
+      httpOnly: true,
+      secure: true,
+      expiresIn: 24 * 60 * 60 * 1000
+    }
+
+    res.cookie("token", token, cookieoptions)
+
+
+    return res.status(201).json(new ApiResponse(201, {
+      token
+    }, "user verification doen succesfully"));
+
+  } catch (error) {
+    throw new ApiError(202, "something went wrong in logging in plz check", false)
+  }
+
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
