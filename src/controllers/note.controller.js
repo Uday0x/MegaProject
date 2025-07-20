c1bb3b68600387945e3a0ba79d646020692a4204
uuuuday <krishnauday200301@gmail.com>
2025-07-20 21:46:44 +0530

    add the latest changes to the code

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -1,8 +1,30 @@
 // boilderplate code
 
+import { ProjectNote } from "../models/note.models.js";
+import { Project } from "../models/project.models.js";
+import { ApiError } from "../utils/api-error.js";
+import { ApiResponse } from "../utils/api-response.js"
+import mongoose from "mongoose"
 const getNotes = async (req, res) => {
   // get all notes
+  const { projectId } = req.params
+
+
+  if(!projectId){
+    throw new ApiError(202,"project id not found",false)
+  }
   
+  const project = await Project.findById(mongoose.Types.ObjectId(projectId))
+
+  const notes=await ProjectNote.find({
+    project:mongoose.Types.ObjectId(projectId)
+  }).populate("createdBy","username email fullname")
+
+
+  return res.status(200).json(
+    new (ApiResponse(200,notes,"notes fected successfully"))
+  )
+
 };
 
 const getNoteById = async (req, res) => {
